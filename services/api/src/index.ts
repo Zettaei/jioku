face412f881bce10ac87cac97194e1f499621075
zt-AURORA <118644042+Zettaei@users.noreply.github.com>
2025-11-30 22:10:03 +0700

    feat(api/ocr): basic ocr endpoint and logics

---
+++ services/api/src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server" 
+import { cors } from "hono/cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import ocr from "./modules/ocr/index.js";
+dotenv.config();
+
+const app = new Hono().basePath("/");
+app.use(cors({
+    credentials: true,
+    origin: process.env["FRONTEND_HOST"] ?? ""
+}));
+
+
+app.route("/ocr", ocr);
+
+
+
+app.onError((err, c) => {
+    console.error(err);
+    return c.text("Server Error", 500);
+});
+
+
+const HOST = "localhost";
+const PORT = 8787;
+serve({
+    fetch: app.fetch,
+    hostname: HOST,
+    port: PORT,
+});
---
+++ services/api/src/modules/ocr/service.ts
@@ -0,0 +1,24 @@
+
+const OCR_ENDPOINT = "/ocr";
+
+
+async function sendImgToOCR(formData: FormData): Promise<OcrResult>
+{
+    const response = await fetch(process.env["OCR_HOST"] + OCR_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            "X-API-KEY": process.env["OCR_API_KEY"]
+        },
+        body: formData
+    });
+
+    if(!response.ok) 
+        throw new Error("OCR server returned " + response.status);
+
+    const data = await response.json() as OcrResult;
+    return data;
+}
+
+export default {
+    sendImgToOCR
+}
